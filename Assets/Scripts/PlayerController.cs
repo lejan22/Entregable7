85ddc3a89b721ca50ee50103c148be56cac586b2
lejan22 <alejandrodl2206@gmail.com>
2022-02-18 11:41:14 +0100

    18 feb

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,7 +15,7 @@ public class PlayerController : MonoBehaviour
 
     //Efecto de sonido del globo rebotando
     public AudioClip Boing;
-    
+    public AudioClip boom;
 
     //Sistema de particulas
     public ParticleSystem explosion;
@@ -48,31 +48,40 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Si no estamos en gameover
         if (!gameOver)
         {
             //Al presionar espacio, el globo volara como un campeon
             if (Input.GetKeyDown(KeyCode.Space))
             {
+                //Asi consigue un impulso
                 playerRigidbody.AddForce(Vector3.up * impulso, ForceMode.Impulse);
-
+                //Reproduce el sonido al saltar
                 playerAudioSource.PlayOneShot(Boing, 0.3f);
             }
 
            
         }
+        //Si llega al techo
         if (transform.position.y > Ylim)
         {
+            //Player no supera el techo
             transform.position = new Vector3(transform.position.x, Ylim, transform.position.z);
 
         }
 
         if (transform.position.y < Floor)
         {
+           //Reproduce sistema de particulas
             explosion.Play();
+            //reproduce el sonido boom(tarda demasado y no puede aparecer ya que el player desaparece antes)
+            playerAudioSource.PlayOneShot(boom, 0.3f);
+           //localizacion boom en el suelo
+            explosion.transform.position = new Vector3(0, 0, 0);
+            //Adios globo
             Destroy(gameObject);
-
-
         }
+
     }
 
     private void GameOver()
